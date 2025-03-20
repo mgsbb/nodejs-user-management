480dad56536fdd37554b485d17c9b274e495acc8
Admin <108695191+mgsbb@users.noreply.github.com>
2025-03-20 19:54:01 +0530

    feat(server): setup cors, cookie parser and other packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
 
 const app = express();
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
