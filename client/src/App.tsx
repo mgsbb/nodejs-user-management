f0e041d7feb54fb8399cb52e2f927717122076d1
Admin <108695191+mgsbb@users.noreply.github.com>
2025-03-21 22:23:42 +0530

    feat(client): add google sign in button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-    return <main>Hello World</main>;
+    const handleLogin = () => {
+        window.location.href = 'http://localhost:3000/auth/google';
+    };
+
+    return <button onClick={handleLogin}>Sign in with Google</button>;
 }
 
 export default App;
