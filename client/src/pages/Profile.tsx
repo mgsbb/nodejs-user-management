db62ab6693ae8799329dc6a0780747e8d814a479
Admin <108695191+mgsbb@users.noreply.github.com>
2025-03-22 23:56:35 +0530

    feat(client): fetch and display user info

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,3 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface IUserInfo {
+    email: string;
+    name: string;
+    picture: string;
+}
+
 export default function Profile() {
-    return <>Welcome</>;
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['users', 'profile'],
+        queryFn: async () => {
+            const res = await axios.get(
+                `http://localhost:3000/api/v1/users/profile`,
+                { withCredentials: true }
+            );
+            const data = await res.data;
+            return data;
+        },
+    });
+
+    if (isError) {
+        return <>Error...</>;
+    }
+
+    if (isLoading) {
+        return <>Loading...</>;
+    }
+
+    const userInfo: IUserInfo = data.user;
+
+    return (
+        <div>
+            <p>Welcome {userInfo?.name}</p>
+            <p>{userInfo?.email}</p>
+            <img src={userInfo?.picture} alt='user profile picture' />
+        </div>
+    );
 }
