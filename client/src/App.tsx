a69dd55a307fc62dac4faab4d2101687ced01054
Admin <108695191+mgsbb@users.noreply.github.com>
2025-03-22 23:56:20 +0530

    feat(client): configure react query client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Login, Profile } from './pages';
 import { ProtectedRoute } from './components/';
 import { AuthProvider } from './context/authContext';
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='login' element={<Login />} />
-                    <Route
-                        index
-                        element={
-                            <ProtectedRoute>
-                                <Profile />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Routes>
-            </BrowserRouter>
-        </AuthProvider>
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='login' element={<Login />} />
+                        <Route
+                            index
+                            element={
+                                <ProtectedRoute>
+                                    <Profile />
+                                </ProtectedRoute>
+                            }
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
+        </QueryClientProvider>
     );
 }
 
