6a5864b63f92f3adb0ccddc341eecc4bcdc4d116
Admin <108695191+mgsbb@users.noreply.github.com>
2025-03-22 16:46:22 +0530

    refactor(server): add google oauth2Client to generate auth url

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
+import { google } from 'googleapis';
+import crypto from 'crypto';
 
 dotenv.config();
 
@@ -22,19 +24,55 @@ const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const JWT_SECRET = process.env.JWT_SECRET!;
 
+const oauth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/auth/google', (req, res) => {
-    const authorizationUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email%20profile&access_type=offline`;
+    // Direct URL without using client library
+    // const authorizationUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email%20profile&access_type=offline`;
+
+    // SCOPES DOCS:  https://developers.google.com/identity/protocols/oauth2/scopes
+
+    // these scopes require app verification
+    // const scopes = [
+    //     'https://www.googleapis.com/auth/drive.metadata.readonly',
+    //     'https://www.googleapis.com/auth/calendar.readonly',
+    // ];
+
+    // const scopes = ['email', 'profile'];
+
+    const scopes = [
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+    ];
+
+    const state = crypto.randomBytes(32).toString('hex');
+
+    const authorizationUrl = oauth2Client.generateAuthUrl({
+        // 'online' (default) or 'offline' (gets refresh_token)
+        access_type: 'offline',
+        /** Pass in the scopes array defined above.
+         * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
+        scope: scopes,
+        // Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes: true,
+        // Include the state parameter to reduce the risk of CSRF attacks.
+        state: state,
+        // prompt: 'consent',
+        // prompt: 'select_account',
+    });
     res.redirect(authorizationUrl);
 });
 
 app.get('/auth/google/callback', async (req, res) => {
-    const code = req.query.code;
-
-    try {
+        // data contains { access_token, expires_in, scope, token_type, id_token }
         const { data } = await axios.post(
             'https://oauth2.googleapis.com/token',
             null,
@@ -50,6 +88,8 @@ app.get('/auth/google/callback', async (req, res) => {
         );
 
         const { access_token } = data;
+
+        // userInfo contains { sub, name, given_name, family_name, picture, email, email_verified }
         const { data: userInfo } = await axios.get(
             'https://www.googleapis.com/oauth2/v3/userinfo',
             {
